a83bc34d43f3e4b382ee0c021fa52b239563390d
AnikHaque <ekramul15-811@diu.edu.bd>
2024-04-16 14:17:03 +0600

    user model created

---
+++ src/models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  name: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  address: String,
+  phone: String,
+  role: String,
+  age: Number,
+  authToken: String,
+});
+
+const userModel = model("users", userSchema);
+
+module.exports = userModel;

654edc3f6f8c0ee819942e5eed24d0e7392f9d3c
AnikHaque <ekramul15-811@diu.edu.bd>
2024-04-21 22:12:27 +0600

    admin otp service created

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 const UserModel = require("../models/user.model");
 const EmailSend = require("../utility/EmailHelper");
 const { EncodeToken } = require("../utility/TokenHelper");
+const adminModel = require("../models/admin.model");
 
 const UserOTPService = async (email) => {
   try {
@@ -16,6 +17,32 @@ const UserOTPService = async (email) => {
 
     // Update OTP in the database
     await UserModel.updateOne(
+      { email: email },
+      { $set: { otp: code } } // Remove 'upsert: true'
+    );
+
+    return {
+      status: "success",
+      message: "6 Digit OTP has been sent",
+      otp: code,
+    }; // Include the OTP in the response
+  } catch (e) {
+    return { status: "fail", message: e };
+  }
+};
+
+const AdminOTPService = async (email) => {
+  try {
+    let code = Math.floor(100000 + Math.random() * 900000);
+
+    let EmailText = `Your Verification Code is= ${code}`;
+    let EmailSubject = "Email Verification";
+
+    // Call EmailSend function (assuming it's defined elsewhere)
+    await EmailSend(email, EmailText, EmailSubject);
+
+    // Update OTP in the database
+    await adminModel.updateOne(
       { email: email },
       { $set: { otp: code } } // Remove 'upsert: true'
     );
@@ -55,7 +82,34 @@ const VerifyOTPService = async (email, otp) => {
   }
 };
 
+const VerifyAdminOTPService = async (email, otp) => {
+  try {
+    otp = otp.toString();
+
+    console.log("Verifying OTP for email:", email);
+    console.log("Provided OTP:", otp);
+
+    const admin = await adminModel.findOne({ email: email, otp: otp });
+
+    console.log("Admin found in database:", admin);
+
+    if (admin) {
+      const token = EncodeToken(email, user._id.toString());
+      await adminModel.updateOne({ email: email }, { $set: { otp: "0" } });
+      await adminModel.updateOne({ email: email }, { $unset: { otp: 1 } }); // Remove the OTP field
+      return { status: "success", message: "Valid OTP", token: token };
+    } else {
+      return { status: "fail", message: "Invalid OTP" };
+    }
+  } catch (error) {
+    console.error("Error verifying OTP:", error);
+    return { status: "fail", message: "Error verifying OTP" };
+  }
+};
+
 module.exports = {
   UserOTPService,
   VerifyOTPService,
+  AdminOTPService,
+  VerifyAdminOTPService,
 };
